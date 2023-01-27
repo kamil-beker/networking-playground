29b22b96af69cc0cf4bf353b1ddc18b73a090095
kamil-beker <kamil@fourtheon.pl>
2023-01-27 14:18:25 +0100

    showip

---
+++ chapter_5.cpp
@@ -0,0 +1,56 @@
+#include <arpa/inet.h>
+#include <iostream>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+    struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    if (argc != 2)
+    {
+        std::cerr << "usage: showip hostname" << std::endl;
+        return 1;
+    }
+
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM; // TCP
+
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0)
+    {
+        std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
+        return 2;
+    }
+
+    std::cout << "IP Addresses for: " << argv[1] << std::endl;
+
+    for (p = res; p != nullptr; p = p->ai_next)
+    {
+        void *addr = nullptr;
+        std::string ipver = "";
+
+        if (p->ai_family == AF_INET)
+        {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        }
+        else
+        {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        std::cout << "--> [" << ipver << "] " << ipstr << std::endl;
+    }
+
+    freeaddrinfo(res);
+
+    return 0;
+}
